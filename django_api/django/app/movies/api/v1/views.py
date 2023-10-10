0428bdd60cdff00026427fded8e22951f6a1ebb4
Dmitry Murzin <dmitry.murzin@exness.com>
2023-10-10 21:42:40 +0300

    Update views.py

--- django_api/django/app/movies/api/v1/views.py
+++ django_api/django/app/movies/api/v1/views.py
@@ -1,7 +1,7 @@
-from django.db.models import Prefetch
+from django.contrib.postgres.aggregates import ArrayAgg
 from django.http import JsonResponse
-# from django.core import serializers
 from django.views.generic.list import BaseListView
+from django.db.models import Q
 
 from movies.models import Filmwork, Genre, PersonFilmwork, PersonFilmworkRole
 
@@ -13,38 +13,23 @@ class MoviesListApi(BaseListView):
     def get_queryset(self):
         movies = (
             Filmwork.objects.all()
-            .prefetch_related(
-                Prefetch('genres', queryset=Genre.objects.all()),
-                Prefetch('personfilmwork_set', queryset=PersonFilmwork.objects.filter(role=PersonFilmworkRole.ACTOR),
-                         to_attr='actors'),
-                Prefetch('personfilmwork_set', queryset=PersonFilmwork.objects.filter(role=PersonFilmworkRole.DIRECTOR),
-                         to_attr='directors'),
-                Prefetch('personfilmwork_set', queryset=PersonFilmwork.objects.filter(role=PersonFilmworkRole.WRITER),
-                         to_attr='writers'),
+            .annotate(
+                genre_names=ArrayAgg('genres__name'),
+                actor_names=ArrayAgg('personfilmwork__person__full_name',
+                                     filter=Q(personfilmwork__role=PersonFilmworkRole.ACTOR)),
+                director_names=ArrayAgg('personfilmwork__person__full_name',
+                                        filter=Q(personfilmwork__role=PersonFilmworkRole.DIRECTOR)),
+                writer_names=ArrayAgg('personfilmwork__person__full_name',
+                                      filter=Q(personfilmwork__role=PersonFilmworkRole.WRITER))
             )
+            .values('id', 'title', 'description', 'creation_date', 'rating', 'type', 'genre_names', 'actor_names',
+                    'director_names', 'writer_names')
         )
         return movies
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        queryset = self.get_queryset()
-        serialized_results = []
-
-        for film_work in queryset:
-            serialized_results.append({
-                'id': str(film_work.id),
-                'title': film_work.title,
-                'description': film_work.description,
-                'creation_date': film_work.creation_date,
-                'rating': film_work.rating,
-                'type': film_work.type,
-                'genres': [genre.name for genre in film_work.genres.all()],
-                'actors': [person_film_work.person.full_name for person_film_work in film_work.actors],
-                'directors': [person_film_work.person.full_name for person_film_work in film_work.directors],
-                'writers': [person_film_work.person.full_name for person_film_work in film_work.writers],
-            })
-
         context = {
-            'results': serialized_results,
+            'results': list(self.get_queryset())
         }
         return context
 
